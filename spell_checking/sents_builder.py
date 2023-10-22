bd606a75c8c3c7c5c645709d7d6c3219a2f578e1
Bozhidar Klouchek <klouchek.bozhidar@gmail.com>
2023-10-23 00:23:29 +0100

    Add spell check main and builder

---
+++ spell_checking/sents_builder.py
@@ -0,0 +1,18 @@
+import os, csv
+
+# Building the style dataset used for style classification task for Slovcho
+cwd = 'C:/Users/klouc/Desktop/slovcho/spell_checking'
+
+with open(f'{cwd}/sents.csv', 'w', newline='', encoding="utf-8") as target:
+    writer = csv.writer(target)
+    writer.writerow(['id', 'sent', 'source', 'file'])
+
+    txtFilesDir = f'{cwd}/data/ultimate_bg_dataset'
+    if(os.path.exists(txtFilesDir)):
+        txtFiles = os.listdir(txtFilesDir)
+        counter = 0
+        for txtFile in txtFiles:
+            with open(f'{cwd}/data/ultimate_bg_dataset/{txtFile}', 'r', encoding="utf-8") as file:
+                for line in file:
+                    writer.writerow([counter, line.replace('\n', ''), 'ultimate_bg_dataset', txtFile])
+                    counter += 1
